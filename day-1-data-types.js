8fcfa60dbb5b8e41c44c42651cae6b9851a08d24
Wibodha Alkegama <w.alkegama@craneware.com>
2018-06-19 10:33:20 +0100

    up to day 2 solutions

---
+++ day-1-data-types.js
@@ -0,0 +1,18 @@
+
+    // Declare second integer, double, and String variables.
+    var w;
+    var a;
+    var j;
+
+    // Read and save an integer, double, and String to your variables.
+    w = parseInt(readLine());
+    a = parseFloat(readLine());
+    j = readLine();
+    // Print the sum of both integer variables on a new line.
+    console.log(w+i);
+
+    // Print the sum of the double variables on a new line.
+    console.log((d+a).toFixed(1));
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    console.log(s+j);
